8f2f1a78c185b6687d83d1ffb33e92dee7096607
Vago Aleksandr <vgoalex666@mail.ru>
2022-05-26 20:34:36 +0300

    feat: completed task with dots

--- Food/js/slider.js
+++ Food/js/slider.js
@@ -38,25 +38,25 @@ const simpleSlider = () => {
 };
 
 export const carousel = () => {
-  const slides = document.querySelectorAll('.offer__slide'),
+  const slider = document.querySelector('.offer__slider'),
+    slides = document.querySelectorAll('.offer__slide'),
     slidesWrapper = document.querySelector('.offer__slider-wrapper'),
     slidesField = document.querySelector('.offer__slider-inner'),
     btnNext = document.querySelector('.offer__slider-next'),
     btnPrev = document.querySelector('.offer__slider-prev'),
     current = document.querySelector('#current'),
     total = document.querySelector('#total'),
-    widthWrapper = window.getComputedStyle(slidesWrapper).width;
+    widthWrapper = window.getComputedStyle(slidesWrapper).width,
+    dotsWrapper = document.querySelector('.carousel-indicators');
 
+  let dots = [];
   let slideIndex = 1;
   let offset = 0;
 
-  if (slides.length < 10) {
-    total.textContent = `0${slides.length}`;
-    current.textContent = `0${slideIndex}`;
-  } else {
-    total.textContent = slides.length;
-    current.textContent = slideIndex;
-  }
+  checkSlide(slides, current, total, slideIndex);
+
+  slider.style.position = 'relative';
+  createDots(dotsWrapper, slides.length);
 
   slidesWrapper.style.overflow = 'hidden';
 
@@ -86,6 +86,8 @@ export const carousel = () => {
     slideIndex < 10
       ? (current.textContent = `0${slideIndex}`)
       : (current.textContent = slideIndex);
+
+    activeDot(dots, slideIndex);
   });
 
   btnPrev.addEventListener('click', () => {
@@ -106,5 +108,54 @@ export const carousel = () => {
     slideIndex < 10
       ? (current.textContent = `0${slideIndex}`)
       : (current.textContent = slideIndex);
+
+    activeDot(dots, slideIndex);
   });
+
+  dots.forEach((dot) => {
+    dot.addEventListener('click', (e) => {
+      const slideTo = e.target.getAttribute('data-slide-to');
+
+      slideIndex = slideTo;
+      offset = parseFloat(widthWrapper) * (slideTo - 1);
+
+      slidesField.style.transform = `translateX(-${offset}px)`;
+
+      checkSlide(slides, current, total, slideIndex);
+
+      activeDot(dots, slideIndex);
+    });
+  });
+
+  function createDots(wrapper, length) {
+    for (let i = 1; i <= length; i++) {
+      const dot = document.createElement('div');
+      dot.classList.add('dot');
+      dot.setAttribute('data-slide-to', i);
+
+      if (i === 1) {
+        dot.style.opacity = 1;
+      }
+
+      wrapper.append(dot);
+      dots.push(dot);
+    }
+  }
+
+  activeDot(dots, slideIndex);
+
+  function activeDot(arr, index) {
+    arr.forEach((dot) => (dot.style.opacity = 0.5));
+    arr[index - 1].style.opacity = 1;
+  }
+
+  function checkSlide(slides, currentSlide, totalSlides, index) {
+    if (slides.length < 10) {
+      totalSlides.textContent = `0${slides.length}`;
+      currentSlide.textContent = `0${index}`;
+    } else {
+      totalSlides.textContent = slides.length;
+      currentSlide.textContent = index;
+    }
+  }
 };
