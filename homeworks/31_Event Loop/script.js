71cff3a112310a2e89ec10637cd883203d5f3126
Vago Aleksandr <vgoalex666@mail.ru>
2022-05-30 15:13:05 +0300

    feat: learned Event loop!

---
+++ homeworks/31_Event Loop/script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// console.log('1');
+
+// setTimeout(() => {
+//   console.log('timeout_1st');
+// }, 100);
+
+// setTimeout(() => {
+//   console.log('timeout_2nd');
+// }, 100);
+
+// console.log('2');
+setTimeout(() => {
+  console.log(1);
+}, 4);
+
+console.log('2');
